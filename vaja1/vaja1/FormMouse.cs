d084ffa72d75f1cc914e6e7db6a3827e5decd0cd
Rok Rozman <rok.rozman2@student.um.si>
2020-03-24 13:20:04 +0100

    Izracuna razdaljo med dvema tockama

--- vaja1/vaja1/FormMouse.cs
+++ vaja1/vaja1/FormMouse.cs
@@ -21,7 +21,7 @@ namespace vaja1
         {
             label1.Text = "X: " + e.X + " Y: " + e.Y;
         }
-        int x1 = 0, y1 = 0, x2 = 0, y2 = 0, x3 = 0, y3 = 0, x4 = 0, y4 = 0; /// koordinate
+        float x1 = 0, y1 = 0, x2 = 0, y2 = 0, x3 = 0, y3 = 0, x4 = 0, y4 = 0; /// koordinate
         int stevec = 0; /// koliko krat smo ze stisnili
         int stevilo_tock = 2; /// koliko tock bomo narisali (3 je tocka in daljica, 4 sta 2 daljici)
         private void FormMouse_MouseClick(object sender, MouseEventArgs e)
@@ -55,14 +55,23 @@ namespace vaja1
                     x1 = e.X;
                     y1 = e.Y;
                     g.FillRectangle(aBrush, x1, y1, 2, 2); // narisemo tocko
+                    label2.Text = "Tocka 1: x1: " + x1 + " y1: " + y1;
                     stevec++;
                 }
                 else
                 {
                     x2 = e.X;
                     y2 = e.Y;
+                    label3.Text = "Tocka 2: x2: " + x2 + " y2: " + y2;
                     g.FillRectangle(aBrush, x2, y2, 2, 2); // narisemo tocko
                     stevec = 0;
+
+                    /// izracunamo razdaljo
+                    double razdalja;
+                    razdalja = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+                    g.DrawLine(pen, x1, y1, x2, y2);// narisemo crto med njima
+                    MessageBox.Show("Razdalja je: " + razdalja);
+
                 }
             }
             else if (stevilo_tock == 3)
@@ -71,6 +80,7 @@ namespace vaja1
                 {
                     x1 = e.X;
                     y1 = e.Y;
+                    label2.Text = "Tocka 1: x1: " + x1 + " y1: " + y1;
                     g.FillRectangle(aBrush, x1, y1, 2, 2); // narisemo tocko
                     stevec++;
                 }
@@ -78,6 +88,7 @@ namespace vaja1
                 {
                     x2 = e.X;
                     y2 = e.Y;
+                    label3.Text = "Tocka 2: x2: " + x2 + " y2: " + y2;
                     g.FillRectangle(aBrush, x2, y2, 2, 2); /// narisemo tocko
                     stevec++;
                 }
@@ -85,6 +96,7 @@ namespace vaja1
                 {
                     x3 = e.X;
                     y3 = e.Y;
+                    label4.Text = "Tocka 3: x3: " + x3 + " y3: " + y3;
                     g.FillRectangle(aBrush, x3, y3, 2, 2); // narisemo tocko
                     g.DrawLine(pen, x2, y2, x3, y3);// narisemo crto med njima
                     stevec = 0;
@@ -96,6 +108,7 @@ namespace vaja1
                 {
                     x1 = e.X;
                     y1 = e.Y;
+                    label2.Text = "Tocka 1: x1: " + x1 + " y1: " + y1;
                     g.FillRectangle(aBrush, x1, y1, 2, 2); // narisemo tocko
                     stevec++;
                 }
@@ -103,6 +116,7 @@ namespace vaja1
                 {
                     x2 = e.X;
                     y2 = e.Y;
+                    label3.Text = "Tocka 2: x2: " + x2 + " y2: " + y2;
                     g.FillRectangle(aBrush, x2, y2, 2, 2); // narisemo tocko
                     g.DrawLine(pen, x1, y1, x2, y2); // narisemo crto med njima
                     stevec++;
@@ -111,6 +125,7 @@ namespace vaja1
                 {
                     x3 = e.X;
                     y3 = e.Y;
+                    label4.Text = "Tocka 3: x3: " + x3 + " y3: " + y3;
                     g.FillRectangle(aBrush, x3, y3, 2, 2); // narisemo tocko
                     stevec++;
                 }
@@ -118,16 +133,17 @@ namespace vaja1
                 {
                     x4 = e.X;
                     y4 = e.Y;
+                    label5.Text = "Tocka 4: x4: " + x4 + " y4: " + y4;
                     g.FillRectangle(aBrush, x4, y4, 2, 2); // narisemo tocko
                     g.DrawLine(pen, x3, y3, x4, y4); // narisemo daljico
                     stevec = 0;
                 }
             }
             /// Tocka 4: x4: 0 y4: 0
-            label2.Text = "Tocka 1: x1: " + x1 + " y1: " + y1;
-            label3.Text = "Tocka 2: x2: " + x2 + " y2: " + y2;
-            label4.Text = "Tocka 3: x3: " + x3 + " y3: " + y3;
-            label5.Text = "Tocka 4: x4: " + x4 + " y4: " + y4;
+            //label2.Text = "Tocka 1: x1: " + x1 + " y1: " + y1;
+            //label3.Text = "Tocka 2: x2: " + x2 + " y2: " + y2;
+            //label4.Text = "Tocka 3: x3: " + x3 + " y3: " + y3;
+            //label5.Text = "Tocka 4: x4: " + x4 + " y4: " + y4;
 
            
             
