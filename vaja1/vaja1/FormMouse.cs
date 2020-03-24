d024c03c7e3046a61c3172c43565f480353019e4
Rok Rozman <rok.rozman2@student.um.si>
2020-03-24 12:56:45 +0100

    dodal da brise tocke

--- vaja1/vaja1/FormMouse.cs
+++ vaja1/vaja1/FormMouse.cs
@@ -26,7 +26,10 @@ namespace vaja1
         int stevilo_tock = 2; /// koliko tock bomo narisali (3 je tocka in daljica, 4 sta 2 daljici)
         private void FormMouse_MouseClick(object sender, MouseEventArgs e)
         {
-            
+            Brush aBrush = (Brush)Brushes.Black;
+            Graphics g = this.CreateGraphics();
+            ///g.Clear(Color.White); // pocistimo tocke
+            if (stevec == 0) { g.Clear(Color.White); }
             /// preverimo kero opcijo smo izbrali
             if (radioButton1.Checked == true)
             {
@@ -47,12 +50,14 @@ namespace vaja1
                 {
                     x1 = e.X;
                     y1 = e.Y;
+                    g.FillRectangle(aBrush, x1, y1, 2, 2); // narisemo tocko
                     stevec++;
                 }
                 else
                 {
                     x2 = e.X;
                     y2 = e.Y;
+                    g.FillRectangle(aBrush, x2, y2, 2, 2); // narisemo tocko
                     stevec = 0;
                 }
             }
@@ -62,18 +67,22 @@ namespace vaja1
                 {
                     x1 = e.X;
                     y1 = e.Y;
+                    g.FillRectangle(aBrush, x1, y1, 2, 2); // narisemo tocko
                     stevec++;
                 }
                 else if (stevec == 1)
                 {
                     x2 = e.X;
                     y2 = e.Y;
+                    g.FillRectangle(aBrush, x2, y2, 2, 2); /// narisemo tocko
                     stevec++;
                 }
                 else
                 {
                     x3 = e.X;
                     y3 = e.Y;
+                    g.FillRectangle(aBrush, x3, y3, 2, 2); // narisemo tocko
+                    // narisemo crto med njima
                     stevec = 0;
                 }
             }
@@ -83,24 +92,30 @@ namespace vaja1
                 {
                     x1 = e.X;
                     y1 = e.Y;
+                    g.FillRectangle(aBrush, x1, y1, 2, 2); // narisemo tocko
                     stevec++;
                 }
                 else if (stevec == 1)
                 {
                     x2 = e.X;
                     y2 = e.Y;
+                    g.FillRectangle(aBrush, x2, y2, 2, 2); // narisemo tocko
+                    // narisemo crto med njima
                     stevec++;
                 }
                 else if(stevec == 2)
                 {
                     x3 = e.X;
                     y3 = e.Y;
+                    g.FillRectangle(aBrush, x3, y3, 2, 2); // narisemo tocko
                     stevec++;
                 }
                 else
                 {
                     x4 = e.X;
                     y4 = e.Y;
+                    g.FillRectangle(aBrush, x4, y4, 2, 2); // narisemo tocko
+                    // narisemo daljico
                     stevec = 0;
                 }
             }
@@ -109,6 +124,10 @@ namespace vaja1
             label3.Text = "Tocka 2: x2: " + x2 + " y2: " + y2;
             label4.Text = "Tocka 3: x3: " + x3 + " y3: " + y3;
             label5.Text = "Tocka 4: x4: " + x4 + " y4: " + y4;
+
+           
+            
         }
+
     }
 }
