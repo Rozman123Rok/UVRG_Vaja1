e1525b8b1e55577b53c4a5a5e5a278d48f15789b
Rok Rozman <rok.rozman2@student.um.si>
2020-03-24 11:22:33 +0100

    Zacetno stanje in dodal da s klikom dolocimo do 4 tocke

---
+++ vaja1/vaja1/FormMouse.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace vaja1
+{
+    partial class FormMouse
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(550, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            this.label1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.label1_MouseClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(649, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tocka 1: x1: 0 y1: 0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(649, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tocka 2: x2: 0 y2: 0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(649, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Tocka 3: x3: 0 y3: 0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(649, 150);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(102, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Tocka 4: x4: 0 y4: 0";
+            // 
+            // FormMouse
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormMouse";
+            this.Text = "FormMouse";
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.FormMouse_MouseClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ vaja1/vaja1/FormMouse.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace vaja1
+{
+    public partial class FormMouse : Form
+    {
+        public FormMouse()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_MouseClick(object sender, MouseEventArgs e)
+        {
+            label1.Text = "X: " + e.X + " Y: " + e.Y;
+        }
+        int x1 = 0, y1 = 0, x2 = 0, y2 = 0, x3 = 0, y3 = 0, x4 = 0, y4 = 0; /// koordinate
+        int stevec = 0; /// koliko krat smo ze stisnili
+        int stevilo_tock = 2; /// koliko tock bomo narisali (3 je tocka in daljica, 4 sta 2 daljici)
+        private void FormMouse_MouseClick(object sender, MouseEventArgs e)
+        {
+            //int x1, y1, x2, y2;
+            ///label1.Text = "X: " + e.X + " Y: " + e.Y;
+            if (stevilo_tock == 2)
+            {
+                if (stevec == 0)
+                {
+                    x1 = e.X;
+                    y1 = e.Y;
+                    stevec++;
+                }
+                else
+                {
+                    x2 = e.X;
+                    y2 = e.Y;
+                    stevec = 0;
+                }
+            }
+            else if (stevilo_tock == 3)
+            {
+                if (stevec == 0)
+                {
+                    x1 = e.X;
+                    y1 = e.Y;
+                    stevec++;
+                }
+                else if (stevec == 1)
+                {
+                    x2 = e.X;
+                    y2 = e.Y;
+                    stevec++;
+                }
+                else
+                {
+                    x3 = e.X;
+                    y3 = e.Y;
+                    stevec = 0;
+                }
+            }
+            else {
+                ///stevilo tock je 4
+                if (stevec == 0)
+                {
+                    x1 = e.X;
+                    y1 = e.Y;
+                    stevec++;
+                }
+                else if (stevec == 1)
+                {
+                    x2 = e.X;
+                    y2 = e.Y;
+                    stevec++;
+                }
+                else if(stevec == 2)
+                {
+                    x3 = e.X;
+                    y3 = e.Y;
+                    stevec++;
+                }
+                else
+                {
+                    x4 = e.X;
+                    y4 = e.Y;
+                    stevec = 0;
+                }
+            }
+            /// Tocka 4: x4: 0 y4: 0
+            label2.Text = "Tocka 1: x1: " + x1 + " y1: " + y1;
+            label3.Text = "Tocka 2: x2: " + x2 + " y2: " + y2;
+            label4.Text = "Tocka 3: x3: " + x3 + " y3: " + y3;
+            label5.Text = "Tocka 4: x4: " + x4 + " y4: " + y4;
+        }
+    }
+}
