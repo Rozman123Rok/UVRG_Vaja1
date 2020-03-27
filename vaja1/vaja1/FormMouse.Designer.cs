2a2beba9514f42cf36af8513584cec41a4dc876d
Rok Rozman <rok.rozman3@gmail.com>
2020-03-27 18:36:31 +0100

    Dodal da lahko vpises koordinate za tocke

--- vaja1/vaja1/FormMouse.Designer.cs
+++ vaja1/vaja1/FormMouse.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
@@ -37,17 +36,25 @@
             this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.radioButton3 = new System.Windows.Forms.RadioButton();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(550, 26);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
-            this.label1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.label1_MouseClick);
             // 
             // label2
             // 
@@ -121,10 +128,173 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label13);
+            this.panel1.Controls.Add(this.label12);
+            this.panel1.Controls.Add(this.label11);
+            this.panel1.Controls.Add(this.label10);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.textBox8);
+            this.panel1.Controls.Add(this.textBox7);
+            this.panel1.Controls.Add(this.textBox6);
+            this.panel1.Controls.Add(this.textBox5);
+            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
             this.panel1.Location = new System.Drawing.Point(606, -5);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(200, 462);
             this.panel1.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(130, 270);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(52, 20);
+            this.textBox2.TabIndex = 10;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(37, 270);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(52, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(10, 273);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(21, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "x1:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(103, 273);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(21, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "y1:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(37, 296);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(52, 20);
+            this.textBox3.TabIndex = 13;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(130, 296);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(52, 20);
+            this.textBox4.TabIndex = 14;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(37, 322);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(52, 20);
+            this.textBox5.TabIndex = 10;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(130, 322);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(52, 20);
+            this.textBox6.TabIndex = 15;
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(37, 348);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(52, 20);
+            this.textBox7.TabIndex = 10;
+            this.textBox7.TextChanged += new System.EventHandler(this.textBox7_TextChanged);
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(130, 348);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(52, 20);
+            this.textBox8.TabIndex = 10;
+            this.textBox8.TextChanged += new System.EventHandler(this.textBox8_TextChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(10, 299);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(21, 13);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "x2:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(103, 299);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(21, 13);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "y2:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(10, 325);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(21, 13);
+            this.label10.TabIndex = 12;
+            this.label10.Text = "x3:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(103, 325);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(21, 13);
+            this.label11.TabIndex = 17;
+            this.label11.Text = "y3:";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(10, 351);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(21, 13);
+            this.label12.TabIndex = 12;
+            this.label12.Text = "x4:";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(103, 351);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(21, 13);
+            this.label13.TabIndex = 18;
+            this.label13.Text = "y4:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(70, 395);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Narisi tocke";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // FormMouse
             // 
@@ -138,19 +308,18 @@
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.panel1);
             this.Name = "FormMouse";
             this.Text = "FormMouse";
             this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.FormMouse_MouseClick);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
@@ -159,5 +328,22 @@
         private System.Windows.Forms.RadioButton radioButton2;
         private System.Windows.Forms.RadioButton radioButton3;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
     }
 }
--- vaja1/vaja1/FormMouse.cs
+++ vaja1/vaja1/FormMouse.cs
@@ -15,15 +15,188 @@ namespace vaja1
         public FormMouse()
         {
             InitializeComponent();
+            
         }
-
-        private void label1_MouseClick(object sender, MouseEventArgs e)
-        {
-            label1.Text = "X: " + e.X + " Y: " + e.Y;
-        }
-        float x1 = 0, y1 = 0, x2 = 0, y2 = 0, x3 = 0, y3 = 0, x4 = 0, y4 = 0; /// koordinate
+        
+        float x1 = 0, y1 = 0, x2 = 0, y2 = 0, x3 = 0, y3 = 0, x4 = 0, y4 = 0; // koordinate tock
+        bool kx1 = false, ky1 = false, kx2 = false, ky2 = false, kx3 = false, ky3 = false, kx4 = false, ky4 = false; /// ce smo vpisali te koordinate
         int stevec = 0; /// koliko krat smo ze stisnili
         int stevilo_tock = 2; /// koliko tock bomo narisali (3 je tocka in daljica, 4 sta 2 daljici)
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            int i = int.Parse(textBox1.Text);
+            //MessageBox.Show("Test: " + i);
+            x1 = i;
+            kx1 = true;
+        }
+
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            int i = int.Parse(textBox2.Text);
+            //MessageBox.Show("Test: " + i);
+            y1 = i;
+            ky1 = true;
+        }
+
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            int i = int.Parse(textBox3.Text);
+            //MessageBox.Show("Test: " + i);
+            x2 = i;
+            kx2 = true;
+        }
+
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            int i = int.Parse(textBox4.Text);
+            //MessageBox.Show("Test: " + i);
+            y2 = i;
+            ky2 = true;
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            int i = int.Parse(textBox5.Text);
+            //MessageBox.Show("Test: " + i);
+            x3 = i;
+            kx3 = true;
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+            int i = int.Parse(textBox6.Text);
+            //MessageBox.Show("Test: " + i);
+            y3 = i;
+            ky3 = true;
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+            int i = int.Parse(textBox7.Text);
+            //MessageBox.Show("Test: " + i);
+            x4 = i;
+            kx4 = true;
+        }
+
+        private void textBox8_TextChanged(object sender, EventArgs e)
+        {
+            int i = int.Parse(textBox8.Text);
+            //MessageBox.Show("Test: " + i);
+            y4 = i;
+            ky4 = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Brush aBrush = (Brush)Brushes.Black; // za risanje tock
+            Pen pen = new Pen(ForeColor); // za risanje daljic med tockama
+            Graphics g = this.CreateGraphics();
+
+            /// ko smo vpisali vse potrebne koordinate da narisemo tocke
+            if (stevec == 0)
+            {
+                g.Clear(Color.White); // pocistim vse tocke in daljice ko je zacetek novih koordinat
+            }
+            /// preverimo kero opcijo smo izbrali
+            if (radioButton1.Checked == true)
+            {
+                stevilo_tock = 2;
+            }
+            else if (radioButton2.Checked == true)
+            {
+                stevilo_tock = 3;
+            }
+            else
+            {
+                stevilo_tock = 4;
+            }
+
+
+
+            if (stevilo_tock == 2)
+            {
+                // bomo narisali 2 tocki
+                if (kx1 && ky1 && kx2 && ky2)
+                {
+                    /// imamo vse koordinate za obe tocki
+                    /// narisemo prvo tocko
+                    g.FillRectangle(aBrush, x1, y1, 2, 2); // narisemo tocko
+                    label2.Text = "Tocka 1: x1: " + x1 + " y1: " + y1;
+                    stevec++;
+                    /// narisemo drugo tocko
+                    label3.Text = "Tocka 2: x2: " + x2 + " y2: " + y2;
+                    g.FillRectangle(aBrush, x2, y2, 2, 2); // narisemo tocko
+                    stevec = 0;
+                    /// izracunamo razdaljo
+                    double razdalja;
+                    razdalja = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+                    g.DrawLine(pen, x1, y1, x2, y2);// narisemo crto med njima
+                    MessageBox.Show("Razdalja je: " + razdalja);
+                }
+                else
+                {
+                    // nimamo vseh koordinat
+                    MessageBox.Show("Vnesi vse koordinate za tocke");
+                }
+            }
+            else if (stevilo_tock == 3) {
+                // narisemo 3 tocke
+                if (kx1 && ky1 && kx2 && ky2 && kx3 && ky3)
+                {
+                    // prva tocka
+                    label2.Text = "Tocka 1: x1: " + x1 + " y1: " + y1;
+                    g.FillRectangle(aBrush, x1, y1, 2, 2); // narisemo tocko
+                    stevec++;
+                    /// druga tocka
+                    label3.Text = "Tocka 2: x2: " + x2 + " y2: " + y2;
+                    g.FillRectangle(aBrush, x2, y2, 2, 2); /// narisemo tocko
+                    stevec++;
+                    // tretja tocka
+                    label4.Text = "Tocka 3: x3: " + x3 + " y3: " + y3;
+                    g.FillRectangle(aBrush, x3, y3, 2, 2); // narisemo tocko
+                    g.DrawLine(pen, x2, y2, x3, y3);// narisemo crto med njima
+                    stevec = 0;
+
+                }
+                else
+                {
+                    // nimamo vseh koordinat
+                    MessageBox.Show("Vnesi vse koordinate za tocke");
+                }
+            }
+            else{
+                /// stevilo tock je 4
+                if (kx1 && ky1 && kx2 && ky2 && kx3 && ky3 && kx4 && ky4) {
+                    /// prva tocka
+                    label2.Text = "Tocka 1: x1: " + x1 + " y1: " + y1;
+                    g.FillRectangle(aBrush, x1, y1, 2, 2); // narisemo tocko
+                    // druga tocka
+                    label3.Text = "Tocka 2: x2: " + x2 + " y2: " + y2;
+                    g.FillRectangle(aBrush, x2, y2, 2, 2); // narisemo tocko
+                    g.DrawLine(pen, x1, y1, x2, y2); // narisemo crto med njima
+                    // tretja tocka
+                    label4.Text = "Tocka 3: x3: " + x3 + " y3: " + y3;
+                    g.FillRectangle(aBrush, x3, y3, 2, 2); // narisemo tocko
+                    // cetrta tocka
+                    label5.Text = "Tocka 4: x4: " + x4 + " y4: " + y4;
+                    g.FillRectangle(aBrush, x4, y4, 2, 2); // narisemo tocko
+                    g.DrawLine(pen, x3, y3, x4, y4); // narisemo daljico
+                }
+                else
+                {
+                    // nimamo vseh koordinat
+                    MessageBox.Show("Vnesi vse koordinate za tocke");
+                }
+            }
+
+        }
+
+        /// koordinate
+
         private void FormMouse_MouseClick(object sender, MouseEventArgs e)
         {
             Brush aBrush = (Brush)Brushes.Black; // za risanje tock
